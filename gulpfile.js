5fdd2a4e1bf44a1dc8e5345fd3217dc0f3b3c2e4
Olha Soroka <osoroka0802@gmail.com>
2021-12-02 20:44:51 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const sass = require('gulp-sass')(require('sass'));
+
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            baseDir: "src"
+        }
+    })
+})
+
+gulp.task('styles', function(){
+    return gulp.src("src/scss/**/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("src/css"))
+    .pipe(browserSync.stream())
+})
+
+gulp.task('watch', function(){
+    gulp.watch("src/scss/**/*.scss", gulp.parallel("styles"))
+    gulp.watch("src/*.html").on('change', browserSync.reload)
+})
+
+gulp.task('default', gulp.parallel('server','styles','watch'))
